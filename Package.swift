6fb54b8b75a70971b794686480e4665428926c15
Luca Vaccaro <me@lvaccaro.com>
2025-10-05 01:39:34 +0200

    Bump Liquid Wallet Kit to branch boltz_patch
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "LiquidWalletKit", targets: ["lwkFFI", "LiquidWalletKit"]),
     ],
     targets: [
-        .binaryTarget(name: "lwkFFI", url: "https://github.com/Blockstream/lwk-swift/releases/download/0.12.0/lwkFFI.xcframework.zip", checksum: "f997682795fb4619afdfdc11e98b7b3b8a39592db8b9d56159daceaeb40e744a"),
+        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/0.12.5/lwkFFI.xcframework.zip", checksum: "b7ff7a54f85d589c4dd7524385a6fe0e762d3819340681cb1c9680b57e9ba6bc"),
         .target(name: "LiquidWalletKit", dependencies: ["lwkFFI"]),
     ]
 )
