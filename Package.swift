017fcc5a1efc8106a95253650a8053ac0deba61c
Luca Vaccaro <me@lvaccaro.com>
2025-10-05 01:49:05 +0200

    Fix framework path
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "LiquidWalletKit", targets: ["lwkFFI", "LiquidWalletKit"]),
     ],
     targets: [
-        .binaryTarget(name: "lwkFFI", url: "https://github.com/Blockstream/lwk-swift/releases/download/boltz_patch/lwkFFI.xcframework.zip", checksum: "12084ebbe24febf77cd59663623ad97b241196bd97f4a65998bf1f127c5e0024"),
+        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/boltz_patch/lwkFFI.xcframework.zip", checksum: "12084ebbe24febf77cd59663623ad97b241196bd97f4a65998bf1f127c5e0024"),
         .target(name: "LiquidWalletKit", dependencies: ["lwkFFI"]),
     ]
 )
