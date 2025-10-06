3ca0c9a4bce3f7b876ba33af35d23fdbcf2f6a37
Luca Vaccaro <me@lvaccaro.com>
2025-10-06 04:01:44 +0200

    Fix boltz tag
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "LiquidWalletKit", targets: ["lwkFFI", "LiquidWalletKit"]),
     ],
     targets: [
-        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/boltz_patch/lwkFFI.xcframework.zip", checksum: "d7dee258e1025238e0044124905ee2f75d48cd42f516cb9116e18e9221f70121"),
+        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/boltz/lwkFFI.xcframework.zip", checksum: "d7dee258e1025238e0044124905ee2f75d48cd42f516cb9116e18e9221f70121"),
         .target(name: "LiquidWalletKit", dependencies: ["lwkFFI"]),
     ]
 )
