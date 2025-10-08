f6fd08e4231e44adb959e319df040331c51a825c
Luca Vaccaro <me@lvaccaro.com>
2025-10-08 16:06:18 +0200

    bioltz1 tag
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "LiquidWalletKit", targets: ["lwkFFI", "LiquidWalletKit"]),
     ],
     targets: [
-        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/boltz/lwkFFI.xcframework.zip", checksum: "d7dee258e1025238e0044124905ee2f75d48cd42f516cb9116e18e9221f70121"),
+        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/boltz1/lwkFFI.xcframework.zip", checksum: "0807380800cb1fe609c19a96e672887db4623e7af36070dd6db8244b929370d2"),
         .target(name: "LiquidWalletKit", dependencies: ["lwkFFI"]),
     ]
 )
