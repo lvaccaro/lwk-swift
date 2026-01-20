2abb1cdbf3b36b5b3b500777ef8932294e94e1e1
Luca Vaccaro <me@lvaccaro.com>
2026-01-20 03:58:42 +0100

    0.13.0-2
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "LiquidWalletKit", targets: ["lwkFFI", "LiquidWalletKit"]),
     ],
     targets: [
-        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/0.13.0-1/lwkFFI.xcframework.zip", checksum: "3b3c4d94cf5fd8a2b0277648760efa552c8474c02c2bb237875672c6c2700100"),
+        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/0.13.0-2/lwkFFI.xcframework.zip", checksum: "08e97a5fffd10513f5e6c02d532e42a40e894733f8bbc65e6072a4a5686288b8"),
         .target(name: "LiquidWalletKit", dependencies: ["lwkFFI"]),
     ]
 )
