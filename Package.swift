52780443357485f6d909ac867535abed97af00ef
Luca Vaccaro <me@lvaccaro.com>
2026-04-23 17:44:13 +0200

    Bump Liquid Wallet Kit to version 0.16.0-2
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "LiquidWalletKit", targets: ["lwkFFI", "LiquidWalletKit"]),
     ],
     targets: [
-        .binaryTarget(name: "lwkFFI", url: "https://github.com/Blockstream/lwk-swift/releases/download/0.16.0-1/lwkFFI.xcframework.zip", checksum: "2318891fb51ed1e727d0eda467c3de0a32656c9dc3daa32c351e9ba36380a875"),
+        .binaryTarget(name: "lwkFFI", url: "https://github.com/lvaccaro/lwk-swift/releases/download/0.16.0-2/lwkFFI.xcframework.zip", checksum: "2318891fb51ed1e727d0eda467c3de0a32656c9dc3daa32c351e9ba36380a875"),
         .target(name: "LiquidWalletKit", dependencies: ["lwkFFI"]),
     ]
 )
